9e3c21e9950faa8f0d7d203dc7716f7b873ce5d3
Suleyman Selcuk <sulomhb@gmail.com>
2022-05-28 23:03:59 +0200

    added price comparison between regnr & aksjonen.no

--- src/functions/functions.py
+++ src/functions/functions.py
@@ -1,16 +1,58 @@
-from lib2to3.pgen2.driver import Driver
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
-import requests
 import time
+import re
 
+def userMenu():
+    resultString = "Resultstring: \n"
+    auctionCarLinks = []
+    userChoice = True
+    while(userChoice):
+        print("""
+        1. Get vehicle registration number from Statens Vegvesen using VIN-number (INSERT VIN-number)
+        2. Get vehicle prices from regnr.no (INSERT Registration Number)
+        3. Get auction prices from auksjonen.no (INSERT URL)
+        4. Get auction cars from auksjonen.no
+        5. Compare auction car with dealer price
+        6. Exit Program
+        """)
 
+        userChoice = input("        I want to: ")
+
+        if(userChoice == "1"):
+            vinNumberInput = input("        Please write the VIN Number: ")
+            getRegNumberOfVehicle(vinNumberInput)
+
+        elif(userChoice == "2"):
+            regNumberInput = input("        Please write the registration number:")
+            getVehiclePrivateAndDealerPrice(regNumberInput,resultString)
+            print(resultString)
+    
+        elif(userChoice == "3"):
+            url = regNumberInput = input("      Please write the auction URL: ")
+            getVehicleAuctionPrice(url, resultString)
+            print(resultString)
+
+        elif (userChoice == "4"):
+            brand = input("      Please write the car brand: ")
+            getAllAuctionCarsGivenBrand(auctionCarLinks,brand)
+            for auctionCarLink in auctionCarLinks:
+                getVehicleAuctionPrice(auctionCarLink, resultString)
+                print(resultString)
+        
+        elif (userChoice == "5"):
+            compareAuctionPriceWithDealerPrice(auctionCarLinks, resultString)
+            print(resultString)
+        
+        elif(userChoice == "6"):
+            userChoice = False
+        
+        else:
+            print("\n Not Valid Choice Try again")  
 
 def getRegNumberOfVehicle(vinNumber):
     
@@ -40,9 +82,7 @@ def getRegNumberOfVehicle(vinNumber):
 
     return regNumber
 
-
-
-def getVehiclePrices(regNumber):
+def getVehiclePrivateAndDealerPrice(regNumber, resultString):
     DRIVER = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
     # Creating the request
@@ -63,27 +103,26 @@ def getVehiclePrices(regNumber):
 
     # Get elements with the "text-price" class.
     allVehiclePrices = WebDriverWait(DRIVER,500).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".text-price")))
-
-    # Print the parent element with children elements as an array (for debugging purposes)
-    print([price.text for price in allVehiclePrices])
-    
+    dealerPrice = 0
+    privatePrice = 0
     #Convert each element to text
     if(len(allVehiclePrices) == 6):
         dealerPrice = allVehiclePrices[3].text
         privatePrice = allVehiclePrices[4].text
         liens = allVehiclePrices[5].text
          # Print out the different prices
-        print(f'''
+        resultString += f""""
         RegNumber: {regNumber}\n
         Dealerprice: {dealerPrice}\n'
         Privateprice: {privatePrice}\n'
-        Liens: {liens}\n''')
+        Liens: {liens}"""
     else:
+
         print(f"Could not get price, please check this URL: https://www.regnr.no/{regNumber}")
     DRIVER.close()
+    return [dealerPrice, privatePrice]
 
-
-def getVehicleAuctionPrice(url):
+def getVehicleAuctionPrice(url, resultString):
     DRIVER = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     # Creating the request
     response = DRIVER.get(url)
@@ -103,52 +142,14 @@ def getVehicleAuctionPrice(url):
 
     vehicleTitle = DRIVER.find_element_by_css_selector(('.crumb-title')).text
 
-    print(f'''
+    resultString += f"""
     {vehicleTitle}
     Total kilometres: {vehicleTotalKilometresDriven} KM
     Highest bid on Auksjonen.no: {vehicleAuctionHighestBid} NOK
-    Car location: {vehicleAuctionLocation}\n\n''')
+    Car location: {vehicleAuctionLocation}\n"""
     DRIVER.close()
 
-def userMenu():
-    userChoice = True
-    while(userChoice):
-        print("""
-        1. Get vehicle registration number from Statens Vegvesen using VIN-number (INSERT VIN-number)
-        2. Get vehicle prices from regnr.no (INSERT Registration Number)
-        3. Get auction prices from auksjonen.no (INSERT URL)
-        4. Get auction cars from auksjonen.no
-        5. Exit Program
-        """)
-
-        userChoice = input("        I want to: ")
-
-        if(userChoice == "1"):
-            vinNumberInput = input("        Please write the VIN Number: ")
-            getRegNumberOfVehicle(vinNumberInput)
-
-        elif(userChoice == "2"):
-            regNumberInput = input("        Please write the registration number: ")
-            getVehiclePrices(regNumberInput)
-    
-        elif(userChoice == "3"):
-            url = regNumberInput = input("      Please write the auction URL: ")
-            getVehicleAuctionPrice(url)
-
-        elif (userChoice == "4"):
-            auctionCarLinks = []
-            brand = input("      Please write the car brand: ")
-            getAllAuctionCarsGivenBrand(auctionCarLinks,brand)
-            for auctionCarLink in auctionCarLinks:
-                getVehicleAuctionPrice(auctionCarLink)
-
-        elif(userChoice == "5"):
-            userChoice = False
-        
-        else:
-            print("\n Not Valid Choice Try again")
-        
-
+    return vehicleAuctionHighestBid
 
 def getAllAuctionCarsGivenBrand(auctionCarLinks, brand):
 
@@ -171,4 +172,35 @@ def getAllAuctionCarsGivenBrand(auctionCarLinks, brand):
         else:
             continue
 
-    #print(auctionCarLinks)
+def getRegNumberOfAuctionCar(url):
+    DRIVER = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    # Creating the request
+    response = DRIVER.get(url)
+    
+    if response:
+        print('(Get RegNumber) Request is successful.')
+    else:
+        print('(Get RegNumber) Request returned an error.')
+
+    auctionCarRegistrationNumber = DRIVER.find_elements_by_css_selector('.ng-binding')
+    for info in auctionCarRegistrationNumber:
+        if re.findall("^[A-Z]{1,2}[0-9]{2,5}$", info.text): # Check if the information is in the Norwegian license plate pattern. [XX00000]
+            regNumber = info.text
+    return regNumber
+
+def compareAuctionPriceWithDealerPrice(auctionCarLinks, resultString):
+    #brand = input('Insert brand you would like to compare the prices to: ')
+    getAllAuctionCarsGivenBrand(auctionCarLinks, "Honda")
+    for auctionCarLink in auctionCarLinks:
+        carHighestBidOnAuction = int(getVehicleAuctionPrice(auctionCarLink, resultString).replace(",-","").replace(" ", ""))
+        regNumberOfAuctionCar = getRegNumberOfAuctionCar(auctionCarLink)
+        carPrivateDealerPrice = getVehiclePrivateAndDealerPrice(regNumberOfAuctionCar, resultString)
+        carPrivateDealerPrice = str(carPrivateDealerPrice[0]).replace(",-","").replace("kr", "").replace(" ", "")
+        resultString += f'Highest bid on auction: {carHighestBidOnAuction} kr\nDealer Price: {carPrivateDealerPrice} kr'
+
+        if int(carHighestBidOnAuction) - int(carPrivateDealerPrice) < 0:
+            resultString += f"Auction car is cheaper by: {int(carPrivateDealerPrice) - int(carHighestBidOnAuction)} kr."
+
+
+userMenu()
